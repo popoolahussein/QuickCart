37e3187f58cd9032fe1ecdc126f1866370ee7027
popoolahussein <popoolakehindehussein@gmail.com>
2024-12-17 08:58:27 +0100

    chore: added nit set-up

---
+++ src/Components/navigationOptions.js
@@ -0,0 +1,9 @@
+const navigationOptions = {
+    animationEnabled: true,
+    transitionSpec: {
+      open: { animation: 'timing', config: { duration: 0 } },
+      close: { animation: 'timing', config: { duration: 0 } },
+    },
+  };
+  
+  export default navigationOptions;
